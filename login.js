4f8e23a33a3e3652eb35a92606ee1b2983f93a82
Minhphu447 <phu.2474802010308@vanlanguni.vn>
2026-09-16 14:32:49 +0700

    Update password check in checkLogin function

--- login.js
+++ login.js
@@ -1,14 +1,21 @@
-
 function checkLogin(user, pass) {
-  return user === "admin" && pass === "123";
+
+  return user === "admin" && pass === "1234";
+
 }
 
 // Hỗ trợ chạy trên trình duyệt
+
 if (typeof window !== "undefined") {
+
   window.checkLogin = checkLogin;
+
 }
 
 // Hỗ trợ Jest / Node.js
+
 if (typeof module !== "undefined" && module.exports) {
+
   module.exports = checkLogin;
+
 }
